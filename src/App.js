7ea10df3beacb19fe941201f8e5663d4f59f97c2
SirEctor <ebenezer.teshome@duke.edu>
2024-06-19 11:42:42 -0400

    add miu + icons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import Button from '@mui/material/Button';
+import LoginIcon from '@mui/icons-material/Login';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test, test, test <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lang-Cards</h1>
+        <Button variant="contained" color="secondary" endIcon={ <LoginIcon /> }>
+          Login
+        </Button>
+        <Button variant="contained" color="secondary" endIcon={ <HowToRegIcon /> }>Register</Button>
+        <h3>What Is Lang-Cards?</h3>
+        <h4>
+          Lang-Cards is a flashcard study/quiz system made to help learn languages.
+        </h4>
+        <p>Created By E. Teshome (2024)</p>
       </header>
     </div>
   );
